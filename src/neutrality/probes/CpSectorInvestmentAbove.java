b19eb1be810e232a3eac87cea5385c2723ab9b49
Kendall Koning <kkoning@gmail.com>
2017-04-29 01:28:32 -0400

    Formalized and abstracted environment probes/tests for re-use across several classes.

---
+++ src/neutrality/probes/CpSectorInvestmentAbove.java
@@ -0,0 +1,38 @@
+package neutrality.probes;
+
+import agency.Agent;
+import neutrality.ContentProvider;
+import neutrality.MarketInfo;
+import neutrality.NeutralityModel;
+
+import static agency.util.Misc.BUG;
+
+/**
+ Created by liara on 4/19/17.
+ */
+public class CpSectorInvestmentAbove implements EnvironmentalContingency {
+
+  EnvironmentalContingency ec = null;
+
+@Override
+public boolean conditionMet(NeutralityModel model, Agent agent, MarketInfo mi, Number parameter) {
+  if (ec != null)
+    return ec.conditionMet(model,agent,mi,parameter);
+
+  try {
+    ContentProvider cp = (ContentProvider) agent;
+    if (cp.isVideo())
+      ec = new CpVideoInvestmentAbove();
+    else
+      ec = new CpOtherInvestmentAbove();
+
+    return ec.conditionMet(model,agent,mi,parameter);
+  } catch (Exception e) {
+    BUG(e.getMessage());
+  }
+
+  // Should be unreachable
+  return false;
+}
+
+}
---
+++ src/neutrality/probes/EnvironmentalContingency.java
@@ -0,0 +1,17 @@
+package neutrality.probes;
+
+import agency.Agent;
+import neutrality.MarketInfo;
+import neutrality.NeutralityModel;
+
+/**
+ Created by liara on 4/19/17.
+ */
+public interface EnvironmentalContingency {
+
+boolean conditionMet(NeutralityModel model,
+                     Agent agent,
+                     MarketInfo mi,
+                     Number parameter);
+
+}
