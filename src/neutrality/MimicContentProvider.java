4881bf2477115a204ae9b74bc74120f764d28ba6
Kendall Koning <kkoning@gmail.com>
2017-04-29 01:31:30 -0400

    Created MimicContentProvider
    MimicContentProvider is a trivial extension to
    DirectlyEncodedContentProvider.  It uses the directly encoded values for
    the first step.  In all subsequent steps, it follows the average market
    values observed in the previous step.

---
+++ src/neutrality/MimicContentProvider.java
@@ -0,0 +1,28 @@
+package neutrality;
+
+import java.util.Optional;
+
+public class MimicContentProvider extends DirectlyEncodedContentProvider {
+@Override
+public Offers.ContentOffer getContentOffer(int step) {
+  if (step == 0)
+    return super.getContentOffer(step);
+
+  double sectorPrice = getSectorPrice(getModel().getMarketInformation(step-1));
+  return new Offers.ContentOffer(step,
+          this,
+          sectorPrice);
+}
+
+@Override
+public void step(NeutralityModel model, int step, Optional<Double> substep) {
+  if (step == 0)
+    super.step(model, step, substep);
+
+  double toInvest = getSectorInvestment(model.getMarketInformation(step-1));
+  makeContentInvestment(step, toInvest);
+
+
+}
+
+}
