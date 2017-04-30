23ef46f3b3f00b57587fcbdf2fe41fdc016150a5
Kendall Koning <kkoning@gmail.com>
2017-04-30 15:07:44 -0400

    Added additional minor documentation to the MimicContentProvider class.

--- src/neutrality/cp/MimicContentProvider.java
+++ src/neutrality/cp/MimicContentProvider.java
@@ -5,26 +5,37 @@ import java.util.Optional;
 import neutrality.NeutralityModel;
 import neutrality.Offers;
 
+/**
+ * MimicContentProvider makes investments in content, and prices that content,
+ * to match the market average in its sector during the previous step. For the
+ * first step, it relies on the genetic encoding of its parent class,
+ * DirectlyEncodedContentProvider.
+ * 
+ * @author liara
+ */
 public class MimicContentProvider extends DirectlyEncodedContentProvider {
+
 @Override
 public Offers.ContentOffer getContentOffer(int step) {
+  // In the first step, use genome.
   if (step == 0)
     return super.getContentOffer(step);
 
-  double sectorPrice = getSectorPrice(getModel().getMarketInformation(step-1));
-  return new Offers.ContentOffer(step,
-          this,
-          sectorPrice);
+  // In all other steps, follow the market.
+  double sectorPrice = getSectorPrice(
+      getModel().getMarketInformation(step - 1));
+  return new Offers.ContentOffer(step, this, sectorPrice);
 }
 
 @Override
 public void step(NeutralityModel model, int step, Optional<Double> substep) {
+  // In the first step, use genome.
   if (step == 0)
     super.step(model, step, substep);
 
-  double toInvest = getSectorInvestment(model.getMarketInformation(step-1));
+  // In all other steps, follow the market.
+  double toInvest = getSectorInvestment(model.getMarketInformation(step - 1));
   makeContentInvestment(step, toInvest);
-
 
 }
 
