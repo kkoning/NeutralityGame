2481eda38e27b45308b3ea6ad5d60b6c6dd8ab64
Kendall Koning <kkoning@gmail.com>
2017-05-03 15:51:15 -0400

    Changed LinearContentProvider to use the facilities provided by VectorIndividual.
    Also now uses linearEqExp style equation, rather than just linearEq.

--- src/neutrality/cp/LinearContentProvider.java
+++ src/neutrality/cp/LinearContentProvider.java
@@ -23,7 +23,7 @@ private static final int INITIAL_INVESTMENT_IDX      = 0;
 private static final int INITIAL_PRICE_IDX           = 1;
 private static final int PRICING_COEFFICIENTS_IDX    = 2;
 private static final int INVESTMENT_COEFFICEINTS_IDX = PRICING_COEFFICIENTS_IDX
-    + NUM_COEFFICIENTS + 1;
+    + VectorIndividual.linearEqExpGenomeLength(NUM_COEFFICIENTS);
 
 @Override
 public Offers.ContentOffer getContentOffer(int step) {
@@ -35,10 +35,10 @@ public Offers.ContentOffer getContentOffer(int step) {
    * the linear equation, just use genome values directly.
    */
   if (step == 0) {
-    price = Math.exp(getManager().getGenomeAt(INITIAL_PRICE_IDX));
+    price = Math.exp(getManager().gene(INITIAL_PRICE_IDX));
   } else {
     double[] envVars = getEnvironmentVariables(step - 1);
-    price = applyLinearEq(PRICING_COEFFICIENTS_IDX, envVars);
+    price = getManager().linearEqExp(PRICING_COEFFICIENTS_IDX, envVars);
   }
 
   return new Offers.ContentOffer(step, this, price);
@@ -49,38 +49,23 @@ public void step(NeutralityModel model, int step, Optional<Double> substep) {
 
   double investment = Double.NaN;
   if (step == 0) {
-    investment = Math.exp(getManager().getGenomeAt(INITIAL_INVESTMENT_IDX));
+    investment = Math.exp(getManager().gene(INITIAL_INVESTMENT_IDX));
   } else {
     double[] envVars = getEnvironmentVariables(step - 1);
-    investment = applyLinearEq(INVESTMENT_COEFFICEINTS_IDX, envVars);
+    investment = getManager().linearEqExp(INVESTMENT_COEFFICEINTS_IDX, envVars);
   }
   makeContentInvestment(step, investment);
 }
 
 private double[] getEnvironmentVariables(int step) {
   double[] toReturn = new double[NUM_COEFFICIENTS];
-  MarketInfo mi = getModel().getMarketInformation(step - 1);
+  MarketInfo mi = getModel().getMarketInformation(step);
   toReturn[0] = getSectorInvestment(mi);
   toReturn[1] = getSectorPrice(mi);
   toReturn[2] = mi.nspNetworkInvestment;
   toReturn[3] = mi.nspBundledPrice;
   toReturn[4] = mi.nspBundlePremium;
   toReturn[5] = mi.nspIXCPrice;
-  return toReturn;
-}
-
-private double applyLinearEq(int start, double[] environmentVariables) {
-  /*
-   * The constant term is raised to e, and is at the first position in the
-   * coeffieients index. All others are multiplied by coefficients from the
-   * genome.
-   */
-  double toReturn = Math.exp(getManager().getGenomeAt(start));
-  for (int i = 0; i < environmentVariables.length; i++) {
-    double var = environmentVariables[i];
-    double coef = getManager().getGenomeAt(start + 1 + i); // 0 is const
-    toReturn += var * coef;
-  }
   return toReturn;
 }
 
