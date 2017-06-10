6bc9bbd679461fb10d4873089f80d5cb9e6362d7
Kendall Koning <kkoning@gmail.com>
2017-06-10 11:00:44 -0400

    Parameterized capital calculation method.
    Whether it is done with log x log or Cobb Douglass is now controlled by
    a configuration variable.  Also lowered mutation rate.

--- src/neutrality/ConsumptionOption.java
+++ src/neutrality/ConsumptionOption.java
@@ -16,13 +16,18 @@ public abstract double getTotalCost();
 public abstract void consume(Consumers consumers, double qty);
 
 public double K() {
-//  double netTerm = Math.pow(network.Kn,network.getModel().tau);
-//  double vidTerm = Math.pow(video.Ka,network.getModel().psi);
-//  double othTerm = Math.pow(other.Ka,network.getModel().psi);
   
-  double netTerm = Math.log(network.Kn + Math.E);
-  double vidTerm = Math.log(video.Ka + Math.E);
-  double othTerm = Math.log(other.Ka + Math.E);
+  double netTerm, vidTerm, othTerm;
+  
+  if (network.getModel().logCapital) {
+    netTerm = Math.log(network.Kn + Math.E);
+    vidTerm = Math.log(video.Ka + Math.E);
+    othTerm = Math.log(other.Ka + Math.E);
+  } else {
+    netTerm = Math.pow(network.Kn,network.getModel().tau);
+    vidTerm = Math.pow(video.Ka,network.getModel().psi);
+    othTerm = Math.pow(other.Ka,network.getModel().psi);
+  }
   
   double vidVal = network.getModel().videoContentValue;
   double othVal = network.getModel().otherContentValue;
